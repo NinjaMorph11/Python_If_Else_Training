8fafa36346b50d0e721dbd38891049856cbc7552
NinjaMorph11 <ninjamorph11@gmail.com>
2024-12-23 16:17:45 +0800

    Update the ForLoop_WithBreaks_Example1.py

--- ForLoop_WithBreaks_Example1.py
+++ ForLoop_WithBreaks_Example1.py
@@ -1,8 +1,7 @@
 # Break statements can break the loop containing it.
 
 # Example 1
-for num in range(0, 10):
-    print(num)
+for num in range(0, 10, 1):
     if num == 5:
         break
     print(f'Iteration: {num}')
